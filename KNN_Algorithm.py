a1129e037512c54241882d67e509a051a2eea601
zeinab-alhwary <82031577+zeinab-alhwary@users.noreply.github.com>
2021-10-19 22:22:11 +0200

    Add files via upload

---
+++ KNN_Algorithm.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 15 19:04:29 2021
+
+@author: hp
+"""
+# KNN algorithm
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_iris
+
+
+iris=load_iris()
+
+print(type(iris))
+print(iris.feature_names)      
+#print(iris.target)
+print(np.unique(iris.target))   #classification types of flower
+
+
+from sklearn.model_selection import train_test_split  #split model to train & test
+
+x=iris.data         # 4 feature
+y=iris.target       # classification
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=.20)
+
+#print(x_train.shape)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn=KNeighborsClassifier(n_neighbors=5)
+
+print(knn.fit(x_train,y_train))
+
+
+y_pred=knn.predict(x_test)
+print(len(y_pred))
+
+from sklearn import metrics
+print(metrics.accuracy_score(y_test,y_pred))
+
+#print(knn.predict([[1,3,2,5]]))
